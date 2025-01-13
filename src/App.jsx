0c3903d203cbffe468980145c1c67aac039e4d8b
monk <akashmauryame@gmail.com>
2025-01-13 14:26:20 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+// Components
+import Header from "./components/Header";
+
+const App = () => {
+  return (
+    <>
+    <Header/>
+    <main>
+      <Hero />
+    </main>
+    </>
+  )
+}
+
+export default App; 
---
+++ src/components/Hero.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+export const Hero = () => {
+  return (
+    <section id="home" className=''>
+        <div className=''>
+            <div>
+                <div className="">
+                    <figure className="">
+                        <img src="" alt="" />
+                    </figure>
+                </div>
+            </div>
+        </div>
+    </section>
+  )
+}
